d79b992160ef7d98ecbd289369406017d4b0d880
Raed Romanov <raid_r@mail.ru>
2019-04-04 10:31:04 +0200

    Log seat reservation

--- backend/server-main/src/main/java/raid/paxteck/server/data/PassengerRepository.java
+++ backend/server-main/src/main/java/raid/paxteck/server/data/PassengerRepository.java
@@ -3,11 +3,9 @@ package raid.paxteck.server.data;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PassengerRepository extends CrudRepository<Passenger, Long> {
 
-    List<Passenger> getPassengersBySeat(String seat);
+    Iterable<Passenger> getPassengersBySeat(String seat);
 
 }
